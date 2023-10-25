174107b351d234bc73f8f0dc9fd18aab10d97c60
Jan Stümmel <7893682+janstuemmel@users.noreply.github.com>
2023-10-25 19:20:21 +0200

    add header hmac and hash

--- test/header.spec.ts
+++ test/header.spec.ts
@@ -1,8 +1,13 @@
 import {expect, it} from 'vitest';
 import {getHeader} from '../lib/header.js';
-import {db1} from './fixtures/test.base64.js';
+import {db1, db2} from './fixtures/test.base64.js';
 import {base64ToBuffer} from './util.js';
 
 it('should parse kdbx4 header', () => {
   expect(getHeader(base64ToBuffer(db1.file))).toMatchSnapshot();
+});
+
+it('foo', () => {
+  const h = getHeader(base64ToBuffer(db2.file));
+  console.log(h);
 });
